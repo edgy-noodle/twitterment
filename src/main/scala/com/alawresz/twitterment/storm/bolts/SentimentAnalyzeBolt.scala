57308a8fbcfc2d54ab18e672be8f9c9fce0c0ce7
edgy-noodle <alexwavresz@gmail.com>
2022-03-22 13:21:38 +0100

    Refactored sentiment analysis into the bolt

--- src/main/scala/com/alawresz/twitterment/storm/bolts/SentimentAnalyzeBolt.scala
+++ src/main/scala/com/alawresz/twitterment/storm/bolts/SentimentAnalyzeBolt.scala
@@ -1,24 +1,57 @@
 package com.alawresz.twitterment.storm.bolts
 
-import com.alawresz.twitterment.nlp.SentimentAnalyzer
 import com.alawresz.twitterment.TweetModel.TweetData
 
 import org.apache.storm.topology.{OutputFieldsDeclarer, IRichBolt}
 import org.apache.storm.task.{OutputCollector, TopologyContext}
 import org.apache.storm.tuple.{Tuple, Values, Fields}
 
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.pipeline.CoreNLPProtos.Sentiment
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
+
 import java.{util => ju}
 
 class SentimentAnalyzeBolt extends IRichBolt {
   var _collector: OutputCollector   = _
   var _conf: ju.Map[String, Object] = _
-  var _analyzer: SentimentAnalyzer  = _
+  var _pipeline: StanfordCoreNLP    = _
+
+  private def findSentiment(text: String): Sentiment = {
+    val (_, sentiment) = extractSentiments(text)
+      .maxBy {
+        case (sentence, _) => sentence.length()
+      }
+    sentiment
+  }
+
+  private def extractSentiments(text: String): List[(String, Sentiment)] = {
+    import scala.collection.JavaConverters._
+
+    val annotation  = _pipeline.process(text)
+    val sentences   = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
+    val trees       = sentences.asScala.map { sentence =>
+      (sentence, sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree]))
+    }
+    trees.map { 
+      case (sentence, tree) => 
+        (sentence.toString(), Sentiment.forNumber(RNNCoreAnnotations.getPredictedClass(tree)))
+    }.toList
+  }
 
   override def prepare(conf: ju.Map[String,Object], context: TopologyContext, 
     collector: OutputCollector): Unit = {
-      _collector  = collector
-      _conf       = conf 
-      _analyzer   = SentimentAnalyzer()
+      val nlpProps  = {
+        val props   = new ju.Properties()
+        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
+        props
+      }
+
+      _collector    = collector
+      _conf         = conf 
+      _pipeline     = new StanfordCoreNLP(nlpProps)
     }
 
   override def execute(tuple: Tuple): Unit = {
@@ -26,7 +59,7 @@ class SentimentAnalyzeBolt extends IRichBolt {
     val lang      = tuple.getStringByField("lang")
     lang match {
       case "en" =>
-        val sentiment = _analyzer.findSentiment(tweet.text).toString()
+        val sentiment = findSentiment(tweet.text).toString()
         println((tweet, sentiment))
         _collector.emit(tuple, new Values(tweet, sentiment))
       case _    =>
